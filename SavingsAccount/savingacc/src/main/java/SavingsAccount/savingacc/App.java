0980c43362df0b2aaed927ee57c9c5b82813f9b1
swati <102591203+sharadsable@users.noreply.github.com>
2022-05-10 15:39:20 +0530

    Add files via upload
    Uploaded by Swati Sable

---
+++ SavingsAccount/savingacc/src/main/java/SavingsAccount/savingacc/App.java
@@ -0,0 +1,13 @@
+package SavingsAccount.savingacc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
